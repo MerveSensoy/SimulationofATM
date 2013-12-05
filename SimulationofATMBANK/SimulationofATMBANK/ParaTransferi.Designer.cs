a4e01f95c72e02b6f0115ae5ed850ef0473b994e
Boğaçhan <bogachangencturk@hotmail.com>
2013-12-06 01:22:55 +0200

    Para Transferi
    Para Transferi ilk sayfa eklendi

---
+++ SimulationofATMBANK/SimulationofATMBANK/ParaTransferi.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SimulationofATMBANK
+{
+    partial class ParaTransferi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button2;
+            System.Windows.Forms.Button button7;
+            System.Windows.Forms.Button button1;
+            System.Windows.Forms.Button sec_1;
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            button2 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            sec_1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(333, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Kart İade";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Anasayfa";
+            // 
+            // button2
+            // 
+            button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            button2.Location = new System.Drawing.Point(12, 189);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(30, 30);
+            button2.TabIndex = 32;
+            button2.Text = ">";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            button7.Location = new System.Drawing.Point(389, 189);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(30, 30);
+            button7.TabIndex = 31;
+            button7.Text = "<";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Tanımlı Hesaba Para Yatırma";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Başka Hesaba Para Yatırma";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            button1.Location = new System.Drawing.Point(389, 49);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(30, 30);
+            button1.TabIndex = 28;
+            button1.Text = "<";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // sec_1
+            // 
+            sec_1.ForeColor = System.Drawing.SystemColors.ControlText;
+            sec_1.Location = new System.Drawing.Point(12, 49);
+            sec_1.Name = "sec_1";
+            sec_1.Size = new System.Drawing.Size(30, 30);
+            sec_1.TabIndex = 27;
+            sec_1.Text = ">";
+            sec_1.UseVisualStyleBackColor = true;
+            // 
+            // ParaTransferi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(button2);
+            this.Controls.Add(button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(button1);
+            this.Controls.Add(sec_1);
+            this.Name = "ParaTransferi";
+            this.Text = "ParaTransferi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
