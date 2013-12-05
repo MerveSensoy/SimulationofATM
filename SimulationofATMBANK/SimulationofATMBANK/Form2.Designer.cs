2e9c15d4080bdd3a576af67d7b86cb00d3d844d1
MerveSensoy <mrvsnsy@gmail.com>
2013-12-05 19:05:29 +0200

    Giriş Sayfası Son Hali
    Form:Şifre Girme

--- SimulationofATMBANK/SimulationofATMBANK/Form2.Designer.cs
+++ SimulationofATMBANK/SimulationofATMBANK/Form2.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Button sec_1;
+            System.Windows.Forms.Button button4;
+            System.Windows.Forms.Button button5;
+            System.Windows.Forms.Button button6;
+            System.Windows.Forms.Button button7;
+            System.Windows.Forms.Button button8;
+            System.Windows.Forms.Button button9;
+            System.Windows.Forms.Button button10;
+            System.Windows.Forms.Button button11;
+            System.Windows.Forms.Button button12;
+            System.Windows.Forms.Button button13;
+            System.Windows.Forms.Button button14;
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            sec_1 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            button10 = new System.Windows.Forms.Button();
+            button11 = new System.Windows.Forms.Button();
+            button12 = new System.Windows.Forms.Button();
+            button13 = new System.Windows.Forms.Button();
+            button14 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -57,7 +81,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(221, 68);
+            this.button1.Location = new System.Drawing.Point(209, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -66,16 +90,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(221, 115);
+            this.button2.Location = new System.Drawing.Point(209, 195);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "Düzeltme";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(221, 160);
+            this.button3.Location = new System.Drawing.Point(209, 227);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
@@ -89,29 +114,144 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
-            // tableLayoutPanel1
+            // sec_1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 59F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(37, 129);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.15254F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.84746F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(178, 128);
-            this.tableLayoutPanel1.TabIndex = 6;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            sec_1.ForeColor = System.Drawing.SystemColors.ControlText;
+            sec_1.Location = new System.Drawing.Point(82, 119);
+            sec_1.Name = "sec_1";
+            sec_1.Size = new System.Drawing.Size(30, 30);
+            sec_1.TabIndex = 6;
+            sec_1.Text = "1";
+            sec_1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            button4.Location = new System.Drawing.Point(118, 119);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(30, 30);
+            button4.TabIndex = 7;
+            button4.Text = "2";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            button5.Location = new System.Drawing.Point(154, 119);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(30, 30);
+            button5.TabIndex = 8;
+            button5.Text = "3";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            button6.Location = new System.Drawing.Point(82, 155);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(30, 30);
+            button6.TabIndex = 9;
+            button6.Text = "4";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            button7.Location = new System.Drawing.Point(118, 155);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(30, 30);
+            button7.TabIndex = 10;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.ForeColor = System.Drawing.SystemColors.ControlText;
+            button8.Location = new System.Drawing.Point(154, 155);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(30, 30);
+            button8.TabIndex = 11;
+            button8.Text = "6";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.ForeColor = System.Drawing.SystemColors.ControlText;
+            button9.Location = new System.Drawing.Point(82, 191);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(30, 30);
+            button9.TabIndex = 12;
+            button9.Text = "7";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.ForeColor = System.Drawing.SystemColors.ControlText;
+            button10.Location = new System.Drawing.Point(118, 191);
+            button10.Name = "button10";
+            button10.Size = new System.Drawing.Size(30, 30);
+            button10.TabIndex = 13;
+            button10.Text = "8";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.ForeColor = System.Drawing.SystemColors.ControlText;
+            button11.Location = new System.Drawing.Point(154, 191);
+            button11.Name = "button11";
+            button11.Size = new System.Drawing.Size(30, 30);
+            button11.TabIndex = 14;
+            button11.Text = "9";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.ForeColor = System.Drawing.SystemColors.ControlText;
+            button12.Location = new System.Drawing.Point(82, 227);
+            button12.Name = "button12";
+            button12.Size = new System.Drawing.Size(30, 30);
+            button12.TabIndex = 15;
+            button12.Text = "-";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.ForeColor = System.Drawing.SystemColors.ControlText;
+            button13.Location = new System.Drawing.Point(118, 227);
+            button13.Name = "button13";
+            button13.Size = new System.Drawing.Size(30, 30);
+            button13.TabIndex = 16;
+            button13.Text = "0";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.ForeColor = System.Drawing.SystemColors.ControlText;
+            button14.Location = new System.Drawing.Point(154, 227);
+            button14.Name = "button14";
+            button14.Size = new System.Drawing.Size(30, 30);
+            button14.TabIndex = 17;
+            button14.Text = "+";
+            button14.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(398, 262);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(button14);
+            this.Controls.Add(button13);
+            this.Controls.Add(button12);
+            this.Controls.Add(button11);
+            this.Controls.Add(button10);
+            this.Controls.Add(button9);
+            this.Controls.Add(button8);
+            this.Controls.Add(button7);
+            this.Controls.Add(button6);
+            this.Controls.Add(button5);
+            this.Controls.Add(button4);
+            this.Controls.Add(sec_1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -120,6 +260,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,7 +274,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
 
     }
 }
--- SimulationofATMBANK/SimulationofATMBANK/Form2.cs
+++ SimulationofATMBANK/SimulationofATMBANK/Form2.cs
@@ -21,5 +21,20 @@ namespace SimulationofATMBANK
         {
 
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
