a909ed4c6bfae27faf53c251f0d761dfe2966bd8
Boğaçhan <bogachangencturk@hotmail.com>
2013-12-07 02:17:53 +0200

    Hızlı Para Çekme ve Para Çekme Makbuz
    Hızlı Para Çekme ve Para Çekme Makbuz designları Eklendi.

---
+++ SimulationofATMBANK/SimulationofATMBANK/50TLHızlıParaÇekme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulationofATMBANK
+{
+    public partial class _50TLHızlıParaÇekme : Form
+    {
+        public _50TLHızlıParaÇekme()
+        {
+            InitializeComponent();
+        }
+
+        private void _50TLHızlıParaÇekme_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimulationofATMBANK/SimulationofATMBANK/ParaÇekmeMakbuz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulationofATMBANK
+{
+    public partial class ParaÇekmeMakbuz : Form
+    {
+        public ParaÇekmeMakbuz()
+        {
+            InitializeComponent();
+        }
+    }
+}
